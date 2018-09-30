e52a2e2f6be288127a219e6cd8b336ac168ea9fc
Jessica Gladstone <jcg321@comcast.net>
2018-09-30 23:08:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,17 @@
 def oxford_comma(array)
   if array.length == 1
-    puts array.join
+    return array.join
     end
     
   if array.length == 2
-    puts array.join(' and ')
+    return array.join(' and ')
   end
   
   if array.length >= 3
     finish = array.last
     array.pop
     string = "#{array.join(', ')}, and #{finish}"
-    puts string
+    return string
 
   end
 end
