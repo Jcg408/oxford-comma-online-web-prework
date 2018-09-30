0b07be29ded51e68ce456b18d262551912e111b9
Jessica Gladstone <jcg321@comcast.net>
2018-09-30 23:07:30 +0000

    automatically backed up by learn

---
+++ lib/oxford_comma.rb
@@ -0,0 +1,17 @@
+def oxford_comma(array)
+  if array.length == 1
+    puts array.join
+    end
+  if array.length == 2
+    puts array.join(' and ')
+  end
+  if array.length >= 3
+    finish = array.last
+    array.pop
+    string = "#{array.join(', ')}, and #{finish}"
+    puts string
+
+
+
+  end
+end
