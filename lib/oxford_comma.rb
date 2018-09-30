8db80710fc017c9357b4609819e26f6099775e67
Jessica Gladstone <jcg321@comcast.net>
2018-09-30 22:05:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1
+ return array.join
+ end
+ if array.length ==2
+  return array.join(' and ')
+ end
+ if array.length ==3
+   return array.join (',') 
+ end
 end
